a6b27b95e10a56f4ea956e94ca2a26fed6e0a1bd
Nicolas Mueller <nicolas.mueller2@gmail.com>
2022-06-01 12:12:15 +0200

    change loss function

--- code/models/gan.py
+++ code/models/gan.py
@@ -54,11 +54,12 @@ class GAN(keras.Model):
         name="discriminator",
         )
         
-    def compile(self, d_optimizer, g_optimizer, loss_fn):
+    def compile(self, d_optimizer, g_optimizer, g_loss_fn, d_loss_fn):
         super(GAN, self).compile()
         self.d_optimizer = d_optimizer
         self.g_optimizer = g_optimizer
-        self.loss_fn = loss_fn
+        self.d_loss_fn = d_loss_fn
+        self.g_loss_fn = g_loss_fn
         self.d_loss_metric = keras.metrics.Mean(name="d_loss")
         self.g_loss_metric = keras.metrics.Mean(name="g_loss")
     @property
@@ -86,7 +87,7 @@ class GAN(keras.Model):
         # Train the discriminator
         with tf.GradientTape() as tape:
             predictions = self.discriminator(combined_images)
-            d_loss = self.loss_fn(labels, predictions)
+            d_loss = self.d_loss_fn(labels, predictions)
         grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
         self.d_optimizer.apply_gradients(
             zip(grads, self.discriminator.trainable_weights)
@@ -102,7 +103,7 @@ class GAN(keras.Model):
         # of the discriminator)!
         with tf.GradientTape() as tape:
             predictions = self.discriminator(self.generator(random_latent_vectors))
-            g_loss = self.loss_fn(misleading_labels, predictions)
+            g_loss = self.g_loss_fn(misleading_labels, predictions)
         grads = tape.gradient(g_loss, self.generator.trainable_weights)
         self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
 
@@ -113,12 +114,13 @@ class GAN(keras.Model):
             "d_loss": self.d_loss_metric.result(),
             "g_loss": self.g_loss_metric.result(),
         }
-    def train(self, data, optimizer, batch_size, epochs, loss_fn, logname):
+    def train(self, data, optimizer, batch_size, epochs, g_loss_fn, d_loss_fn, logname):
         self.reporter.setLogName(logname)
         self.compile(
             d_optimizer=optimizer,
             g_optimizer=optimizer,
-            loss_fn=loss_fn
+            g_loss_fn=g_loss_fn
+            d_loss_fn=d_loss_fn
         )
 
         self.fit(
---
+++ code/models/stylegan.py
@@ -0,0 +1,42 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow as tf
+from gan import GAN
+
+def buildDCGAN(reporter, title, latent_dim):
+    gan = GAN(reporter, title, latent_dim)
+    generator = keras.Sequential(
+        [
+            keras.Input(shape=(latent_dim,)),
+            layers.Dense(8 * 8 * 128),
+            layers.Reshape((8, 8, 128)),
+            layers.Conv2DTranspose(128, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2DTranspose(256, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2DTranspose(512, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2DTranspose(1024, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(1, kernel_size=5, padding="same", activation="sigmoid"),
+        ],
+        name="generator",
+        )
+    discriminator = keras.Sequential(
+        [
+            keras.Input(shape=(128, 128,1)),
+            layers.Conv2D(64, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Conv2D(128, kernel_size=4, strides=2, padding="same"),
+            layers.LeakyReLU(alpha=0.2),
+            layers.Flatten(),
+            layers.Dropout(0.2),
+            layers.Dense(1, activation="sigmoid"),
+        ],
+        name="discriminator",
+        )
+    gan.generator = generator
+    gan.discriminator = discriminator
+    return gan
