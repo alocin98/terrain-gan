817d4b711033965bcb502ebf40b67a0709403fad
Nicolas Mueller <nicolas.mueller2@gmail.com>
2022-07-10 10:02:35 +0200

    fix

--- code/models/gan.py
+++ code/models/gan.py
@@ -125,7 +125,7 @@ class GAN(keras.Model):
         )
 
         self.fit(
-            data, epochs=epochs, callbacks=[self.reporter, keras.callbacks.EarlyStopping(monitor='g_loss', mode='min', min_delta=0.001, patience=50)]
+            data, epochs=epochs, callbacks=[self.reporter, keras.callbacks.EarlyStopping(monitor='g_loss', mode='min', min_delta=0.001, patience=20)]
         )
 
 
